b7b6446938609e5e386f20475be082c0a97dfcd6
francisco drago <franciscodrago1999@gmail.com>
2023-08-20 00:05:53 -0300

    websockets

---
+++ src/app.js
@@ -0,0 +1,37 @@
+import express, { json } from "express"
+import ProductManager from "./managers/ProductManager.js";
+import productsRouter from "./routes/products-router.js";
+import CartManager from "./managers/CartManager.js";
+import cartsRouter from "./routes/carts-router.js";
+import __dirname from "./utils.js";
+import { engine } from "express-handlebars";
+import { Server } from "socket.io";
+import viewsRouter from "./routes/views-router.js";
+
+const manager = new ProductManager("./src/jsons/products.json")
+const cartManager = new CartManager("./src/jsons/cart.json")
+
+const app = express()
+app.use(json())
+
+app.use(express.static(__dirname + '/../public'))
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', __dirname + '/views');
+
+const httpServer = app.listen(8080, ()=>{
+    console.log("Server listening on port 8080.")
+})
+
+const io = new Server(httpServer)
+
+io.on("connection", (socket)=>{
+    console.log("New client connected.")
+})
+
+app.use("/api/products", productsRouter)
+app.use("/api/carts", cartsRouter)
+app.use("/", viewsRouter)
+
+export {manager, cartManager}
